c07ef9632927c228c9cdb7aee8269cb1f0957eb3
Melanie Arellano <melaniesigrid@protonmail.com>
2022-04-07 14:42:32 -0600

    Complete any? and filter methods in MyEnumerable

--- enumerable.rb
+++ enumerable.rb
@@ -4,6 +4,18 @@ module MyEnumerable
     each { |n| result = false unless yield n }
     result
   end
+
+  def any?
+    result = false
+    each { |n| result = true if yield n }
+    result
+  end
+
+  def filter
+    result = []
+    each { |n| result.append(n) if yield n }
+    result
+  end
 end
 
 class MyList
@@ -21,5 +33,14 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
+
+# Test #all?
 puts list.all? {|e| e < 5}
 puts list.all? {|e| e > 5}
+
+# Test #any?
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+
+# Test #filter
+p list.filter {|e| e.even?}
